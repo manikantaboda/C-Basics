e2ad3c5cc84e9a21427de45da8055c1133fb84fa
manikantaboda <92287624+manikantaboda@users.noreply.github.com>
2021-10-20 11:01:48 +0530

    Add files via upload

---
+++ veeru.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+int age;
+float cgpa;
+char initial;
+age = 20;
+cgpa = 9.3;
+initial = 'B';
+printf("%d\n",age);
+printf("%f\n",cgpa);
+printf("%c\n",initial);
+}
