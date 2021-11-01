f9039f58bd7ddf22864a8908f25fb61e72972660
manikantaboda <92287624+manikantaboda@users.noreply.github.com>
2021-11-01 10:19:48 +0530

    Add files via upload

---
+++ arithmetic operations.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+void main()
+{
+	int a,b,c,d,e,f,g,h;
+	a=10;
+	b=2;
+	c=a+b;
+	d=a-b;
+	e=a*b;
+	f=a/b;
+	g=a%b;
+	h=c+d+e+f+g;
+	printf("sum of 10 and 2 is %d\n",c);
+	printf("difference of 10 and 2 is %d\n",d);
+	printf("product of 10 and 2 is %d\n",e);
+	printf("quotient of 10 and 2 is %d\n",f);
+	printf("remainder of 10 and 2 is %d\n",g);
+	printf("sum of all arithmetic operations  is %d\n",h);
+	
+}
