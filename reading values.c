2d21a7c7930df0cc2ce3a0914f1ecdfc59dba48b
manikantaboda <92287624+manikantaboda@users.noreply.github.com>
2021-11-09 10:40:37 +0530

    Add files via upload

---
+++ reading values.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+void main()
+{
+	int a,b,c;
+	 printf("enter the numbers");
+	scanf("%d%d%d",&a, &b, &c);
+	printf("1st number:%d\n",a);
+	printf("2nd number:%d\n",b);
+	printf("3rd number:%d\n",c);
+}
